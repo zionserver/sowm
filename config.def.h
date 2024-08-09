03632f0b9ba05674699ddf026012d02703da8ede
gudrak <63885769+zionserver@users.noreply.github.com>
2024-08-09 16:10:09 -0300

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 #define MOD Mod4Mask
 
 const char* menu[]    = {"dmenu_run",      0};
-const char* term[]    = {"st",             0};
+const char* term[]    = {"lxterminal",     0};
 const char* scrot[]   = {"scr",            0};
 const char* briup[]   = {"bri", "10", "+", 0};
 const char* bridown[] = {"bri", "10", "-", 0};
